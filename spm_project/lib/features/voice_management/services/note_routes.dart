6d65ceed955c3c536d006105a3eda5a28693cdd7
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2024-10-02 12:27:13 +0530

    add note routes

---
+++ spm_project/lib/features/voice_management/services/note_routes.dart
@@ -0,0 +1,10 @@
+import 'package:spm_project/features/voice_management/voice_controller.dart';
+import 'package:go_router/go_router.dart';
+
+final notesRoute = [
+  GoRoute(
+    path: '/notes',
+    name: 'notes',
+    builder: (context, state) => VoiceController(),
+  ),
+];
